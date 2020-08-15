2f1daa6a552aaa738a9f2f5cc25559676135ff54
Matthew Perry <matthew.perry@arcticshores.com>
2020-08-15 14:33:32 +0100

    Balloon Burst - String To Array
    * Add function to convert string to array of integers
    * Add checks for isInteger and isAllIntegers
    * Add case match for user input to convert string to array of
    integers in the case of the input being all integers
    * Add tests

--- balloonburst/src/main/scala/Main.scala
+++ balloonburst/src/main/scala/Main.scala
@@ -3,5 +3,18 @@ import scala.io.StdIn.readLine
 object Main extends App {
   val userInput = getUserInput(readLine())
 
-  def getUserInput(s: String): String = s
+  def getUserInput(s: String): String = {
+    println(s)
+    s
+  }
+
+  def convertStringArray(s: String): Array[Int] = s.split(" ").map(_.toInt)
+
+  def isInteger(s: String): Boolean = s.forall(_.isDigit)
+
+  def isAllIntegers(s: String): Boolean = s.split(" ").forall(isInteger)
+
+  def matchUserInput(f: String => Boolean, s: String) = f(s) match {
+    case true => convertStringArray(s)
+  }
 }
--- balloonburst/src/test/scala/MainSpec.scala
+++ balloonburst/src/test/scala/MainSpec.scala
@@ -11,4 +11,29 @@ class MainSpec extends AnyFlatSpec with Matchers {
       Main.getUserInput(StdIn.readLine()) shouldEqual "test input"
     }
   }
+
+  "convertStringArray" should "convert a list of int strings to an array" in {
+    val in = "1 2 3 4"
+    Main.convertStringArray(in) shouldEqual Array(1, 2, 3, 4)
+  }
+
+  "isInteger" should "return true if the passed string is an integer" in {
+    val in = "1"
+    Main.isInteger(in) shouldEqual true
+  }
+
+  "isInteger" should "return false if the passed string is not an integer" in {
+    val in = "x"
+    Main.isInteger(in) shouldEqual false
+  }
+
+  "isAllIntegers" should "return true if the passed string can be parsed as a list of integers" in {
+    val in = "1 2 3 4"
+    Main.isAllIntegers(in) shouldEqual true
+  }
+
+  "matchUserInput" should "return an array of ints if the passed string contains only integers" in {
+    val in = "1 2 3 4"
+    Main.matchUserInput(Main.isAllIntegers, in) shouldEqual Array(1, 2, 3, 4)
+  }
 }
